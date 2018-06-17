0563632a46277fc6ea1079e1bbbc25ab3b8293ef
Ron Sala <ron.sala@gmail.com>
2018-06-17 20:09:44 +0000

    automatically backed up by learn

--- db/migrate/009_add_day_and_season_to_show.rb
+++ db/migrate/009_add_day_and_season_to_show.rb
@@ -1,5 +1,6 @@
-class AddGenreToShow < ActiveRecord::Migration[4.2]
+class AddDayAndSeasonToShow < ActiveRecord::Migration[4.2]
   def change
-    add_column :shows, :genre, :string
+    add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end
 end
